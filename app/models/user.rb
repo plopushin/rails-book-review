00be87bd3e538a4eab18fb48196b06ffbe645ce2
plopushin <lopushin@ualberta.ca>
2017-03-28 15:27:01 -0600

    Add reviews, editing, and delete functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-
+	has_many :reviews
 	has_many :books
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
